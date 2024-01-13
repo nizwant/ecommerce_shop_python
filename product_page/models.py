898671a66ff31554b152c22bd7523a7d777d9eac
nizwant <mateusznizwantowski@gmail.com>
2024-01-13 17:23:06 +0100

    Add product page functionality

--- product_page/models.py
+++ product_page/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    view_count = models.IntegerField(default=0)
+    description = models.TextField()
+    main_image = models.ImageField(upload_to="Products", default="Products/No-img.jpg")
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    review_score = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
