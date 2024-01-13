74bb0c6a9ab85ce87f18c37d010a49c39cdc7f02
nizwant <mateusznizwantowski@gmail.com>
2024-01-13 18:33:24 +0100

    Update .gitignore and add new templates and views

--- product_page/views.py
+++ product_page/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Product
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    product_list = Product.objects.order_by("name")
+    context = {"product_list": product_list}
+    return render(request, "products/index.html", context)
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, "products/detail.html", {"product": product})
