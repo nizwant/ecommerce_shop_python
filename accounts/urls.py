48db7a4808e0bb8a825340070d154810f4f754f1
nizwant <mateusznizwantowski@gmail.com>
2024-01-14 21:41:31 +0100

    Add user profile update functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("register/", views.register, name="register"),
+    path("profile/update", views.UserProfileUpdateView.as_view(), name="profile"),
+    path("profile/", views.user_profile_detail_view, name="profile_detail"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView
+from .models import UserProfile
+from django import forms
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -11,3 +17,43 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "registration/register.html", {"form": form})
+
+
+@login_required
+def user_profile_detail_view(request):
+    user_profile = request.user.userprofile
+    return render(
+        request, "registration/profile_detail.html", {"user_profile": user_profile}
+    )
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "birth_date",
+            "street_name",
+            "street_number",
+            "house_number",
+            "zip_code",
+            "city",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field_name in self.fields:
+            self.fields[field_name].required = False
+
+
+class UserProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = UserProfile
+    form_class = UserProfileForm
+    template_name = "registration/profile_update.html"
+    success_url = reverse_lazy("profile_detail")
+
+    def get_object(self):
+        return self.request.user.userprofile
