781fec72e73f97a5649399faa6555ba6ed9dd128
nizwant <mateusznizwantowski@gmail.com>
2024-01-14 19:38:40 +0100

    Add accounts app with necessary files and templates

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("products/", include("product_page.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
