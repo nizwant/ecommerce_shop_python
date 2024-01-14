1bce47cd53017aa512db635c30b261963b997b57
nizwant <mateusznizwantowski@gmail.com>
2024-01-14 18:58:07 +0100

    Add password reset form CSS and update password reset form template

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,9 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("products/", include("product_page.urls")),
-]
-
-urlpatterns += [
     path("accounts/", include("django.contrib.auth.urls")),
 ]
 
