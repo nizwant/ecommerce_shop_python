8df64304b15622fbf7a410d17ef26f24ead3b37b
nizwant <mateusznizwantowski@gmail.com>
2024-01-15 20:31:10 +0100

    Add UserProfileForm and update views.py

---
+++ accounts/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "birth_date",
+            "street_name",
+            "street_number",
+            "house_number",
+            "zip_code",
+            "city",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field_name in self.fields:
+            self.fields[field_name].required = False
--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
 from .models import UserProfile
-from django import forms
 from django.contrib.auth.decorators import login_required
+from .forms import UserProfileForm
 
 
 def register(request):
@@ -25,28 +25,6 @@ def user_profile_detail_view(request):
     return render(
         request, "registration/profile_detail.html", {"user_profile": user_profile}
     )
-
-
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = [
-            "first_name",
-            "last_name",
-            "email",
-            "phone_number",
-            "birth_date",
-            "street_name",
-            "street_number",
-            "house_number",
-            "zip_code",
-            "city",
-        ]
-
-    def __init__(self, *args, **kwargs):
-        super(UserProfileForm, self).__init__(*args, **kwargs)
-        for field_name in self.fields:
-            self.fields[field_name].required = False
 
 
 class UserProfileUpdateView(LoginRequiredMixin, UpdateView):
