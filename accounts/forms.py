49f647b2825ee51bd2196e429cf415256fd0d204
nizwant <mateusznizwantowski@gmail.com>
2024-01-27 11:40:27 +0100

    Add zip code validation to UserProfileForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -22,12 +22,21 @@ class UserProfileForm(forms.ModelForm):
         ]
         widgets = {
             "birth_date": forms.DateInput(attrs={"type": "date"}),
+            "zip_code": forms.TextInput(attrs={"placeholder": "xx-xxx"}),
         }
 
     def __init__(self, *args, **kwargs):
         super(UserProfileForm, self).__init__(*args, **kwargs)
         for field_name in self.fields:
             self.fields[field_name].required = False
+
+        self.fields["zip_code"].validators.append(
+            RegexValidator(
+                regex=r"^\d{2}-\d{3}$",
+                message="Zip code must be in the format: xx-xxx",
+                code="invalid_zip_code",
+            )
+        )
 
     def clean_birth_date(self):
         birth_date = self.cleaned_data.get("birth_date")
