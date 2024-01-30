26a68b0421577cd1b45d2211399ac7745fe52359
nizwant <mateusznizwantowski@gmail.com>
2024-01-30 20:49:59 +0100

    Update fade out timing in popup.js and remove font-family in password_reset_confirm.css

--- static/popup.js
+++ static/popup.js
@@ -3,5 +3,5 @@ setTimeout(function () {
     alert.style.opacity = '0';
     setTimeout(function () {
         alert.style.display = 'none';
-    }, 1000); /* Wait for the transition to complete */
-}, 3000); /* Start the fade out after 1 second */
+    }, 1000); 
+}, 3000);
